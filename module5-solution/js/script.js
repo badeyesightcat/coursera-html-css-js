a64e387cf957c0bfbbbef91f4e66cdb06c1287fc
Mee <whitebigbabybird@gmail.com>
2018-12-06 05:18:50 +0900

    trying to catch the concept of the ajax

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -3,6 +3,7 @@ $(function () { // Same as document.addEventListener("DOMContentLoaded"...
   // Same as document.querySelector("#navbarToggle").addEventListener("blur",...
   $("#navbarToggle").blur(function (event) {
     var screenWidth = window.innerWidth;
+    //console.log(screenWidth);
     if (screenWidth < 768) {
       $("#collapsable-nav").collapse('hide');
     }
